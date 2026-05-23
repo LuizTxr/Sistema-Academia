f8f5cfe07d6f28c6cdd040225b66c22f103f4fad
Lucas Fernandes LOQ-E <lucas.adsf2@gmail.com>
2026-05-23 19:33:02 -0300

    config: habilita CORS via variavel de ambiente e adiciona env examples

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+  });
+
   // Validação global
   app.useGlobalPipes(
     new ValidationPipe({
